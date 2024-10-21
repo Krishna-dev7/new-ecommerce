8b89fd006dff88f6b3dd2d236864623a2c480c47
Krishna-dev7 <suprabha205@gmail.com>
2024-10-21 14:27:51 +0530

    corrections

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://new-ecommerce-1-k2dc.onrender.com",
+      '/api': "https://new-ecommerce-1-k2dc.onrender.com",
     }
   }
 })
